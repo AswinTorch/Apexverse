22a6e0ee8a8cfe37bf4fc56e22f33cefc0ea41a3
Aswin Nair <aswintorch@gmail.com>
2017-03-07 01:44:09 +0000

    Added sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -13,3 +13,13 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app64689589@heroku.com',
+  :password => 'botrnrkd4177',
+  :domain => 'sheltered-inlet-36912.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
